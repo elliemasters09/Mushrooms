aacd35685e84a6676c193c6e957d7ac643a45577
elliemasters09 <elliemasters09@gmail.com>
2022-02-08 12:26:38 +0000

    Initial commit

---
+++ src/main/java/com/example/mushroom/MushroomsApplication.java
@@ -0,0 +1,13 @@
+package com.example.mushroom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MushroomsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MushroomsApplication.class, args);
+	}
+
+}
